82fca388e080eb09d75534da96a845b9017f8fcd
terry <munu220@gmail.com>
2023-05-30 13:15:17 +0900

    변수

--- default.js
+++ default.js
@@ -1,24 +1,34 @@
-// type
-console.log(123, typeof 123);
-console.log(123.5, typeof 123.5);
-console.log("123", typeof "123");
-console.log(true, typeof true);
-console.log(false, typeof false);
+var name = "이승훈";
+var age = 29;
+var cgpa = 3.92;
+var lineBreak = "<br>";
+document.write("이름: " + name + lineBreak);
+document.write("나이: " + age + lineBreak);
+document.write("학점: " + cgpa + lineBreak);
 
-var car;
-console.log(car, typeof car);
-var car = "";
-console.log(car, typeof car);
+// // type
+// console.log(123, typeof 123);
+// console.log(123.5, typeof 123.5);
+// console.log("123", typeof "123");
+// console.log(true, typeof true);
+// console.log(false, typeof false);
 
-var person = { firstname: "John", lastname: "Doe", age: 50, eyeColor: "blue" };
-console.log(typeof person, person);
-person = null;
-console.log(typeof person, person);
+// // value
+// var car;
+// console.log(car, typeof car);
+// var car = "";
+// console.log(car, typeof car);
+// var person = { firstname: "John", lastname: "Doe", age: 50, eyeColor: "blue" };
+// console.log(typeof person, person);
+// person = null;
+// console.log(typeof person, person);
 
+// // document write
 // document.write("Hello World!");
 // document.write("<h1>Welcom to JS Program!</h1>");
 // document.write("<h2>Welcom to JS Program!</h2>");
 
+// // console
 // console.debug("Welcom to JS Program!");
 // console.log("Welcom to JS Program!");
 // console.warn("Welcom to JS Program!");
