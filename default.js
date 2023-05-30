f88b578470fe7ede8d41f177dc4ee4817150b998
terry <munu220@gmail.com>
2023-05-30 13:03:10 +0900

    데이터 타입과 값

--- default.js
+++ default.js
@@ -1,12 +1,30 @@
-document.write("Hello World!");
-document.write("<h1>Welcom to JS Program!</h1>");
-document.write("<h2>Welcom to JS Program!</h2>");
+// type
+console.log(123, typeof 123);
+console.log(123.5, typeof 123.5);
+console.log("123", typeof "123");
+console.log(true, typeof true);
+console.log(false, typeof false);
 
-console.debug("Welcom to JS Program!");
-console.log("Welcom to JS Program!");
-console.warn("Welcom to JS Program!");
-console.error("Welcom to JS Program!");
+var car;
+console.log(car, typeof car);
+var car = "";
+console.log(car, typeof car);
 
-alert("Welcom to JS Program!");
-var a = prompt("Enter your name");
-console.log(a)
+var person = { firstname: "John", lastname: "Doe", age: 50, eyeColor: "blue" };
+console.log(typeof person, person);
+person = null;
+console.log(typeof person, person);
+
+// document.write("Hello World!");
+// document.write("<h1>Welcom to JS Program!</h1>");
+// document.write("<h2>Welcom to JS Program!</h2>");
+
+// console.debug("Welcom to JS Program!");
+// console.log("Welcom to JS Program!");
+// console.warn("Welcom to JS Program!");
+// console.error("Welcom to JS Program!");
+
+// // alert
+// alert("Welcom to JS Program!");
+// var a = prompt("Enter your name");
+// console.log(a)
