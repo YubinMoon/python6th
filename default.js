3297f48f336552b80e60eda71db5a393957a5c39
terry <munu220@gmail.com>
2023-05-30 11:24:29 +0900

    출력함수 실습

--- default.js
+++ default.js
@@ -1,1 +1,12 @@
-document.write("Hello World!")
+document.write("Hello World!");
+document.write("<h1>Welcom to JS Program!</h1>");
+document.write("<h2>Welcom to JS Program!</h2>");
+
+console.debug("Welcom to JS Program!");
+console.log("Welcom to JS Program!");
+console.warn("Welcom to JS Program!");
+console.error("Welcom to JS Program!");
+
+alert("Welcom to JS Program!");
+var a = prompt("Enter your name");
+console.log(a)
