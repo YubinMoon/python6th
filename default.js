ddc8bad3b1f0887d7ec385c272239c7242386c54
terry <munu220@gmail.com>
2023-05-31 09:37:53 +0900

    관계 연산자, 논리 연산자

--- default.js
+++ default.js
@@ -1,7 +1,28 @@
-// 온도 변환
-var cels = parseFloat(prompt("섭씨 입력 : "));
-var farn = cels * (9 / 5) + 32;
-document.write("화씨 : " + farn);
+// 비교문
+var num1 = 20;
+var num2 = 10;
+var num3 = "10";
+var num4 = 20;
+var num5 = 15;
+console.log("일반 크기 비교");
+console.log(num1 > num2, num1, ">", num2);
+console.log(num1 >= num2, num1, ">=", num2);
+console.log(num1 < num2, num1, "<", num2);
+console.log(num1 <= num2, num1, "<=", num2);
+console.log("같은지 여부 확인");
+console.log(num1 == num4, num1, "==", num4);
+console.log(num1 != num4, num1, "!=", num4);
+console.log("===");
+console.log(num1 === num3, num1, "===", num3);
+console.log(num2 === num3, num2, "===", num3);
+console.log(num2 == num3, num2, "==", num3);
+console.log(num1 > num2 && num1 < num5, num1, ">", num2, "&&", num1, "<", num5);
+console.log(num1 > num2 || num1 < num5, num1, ">", num2, "||", num1, "<", num5);
+
+// // 온도 변환
+// var cels = parseFloat(prompt("섭씨 입력 : "));
+// var farn = cels * (9 / 5) + 32;
+// document.write("화씨 : " + farn);
 
 // // 소수
 // var base = parseFloat(prompt("Enter 밑변 : "));
