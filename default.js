4994a0048910d1af6d263eba74227b8a26dc1f62
terry <munu220@gmail.com>
2023-05-31 11:27:43 +0900

    while 반복

--- default.js
+++ default.js
@@ -1,40 +1,54 @@
-// switch
-var digit = parseInt(prompt("Enter a digit : "));
-switch (digit) {
-	case 0:
-		document.write("zero");
-		break;
-	case 1:
-		document.write("one");
-		break;
-	case 2:
-		document.write("two");
-		break;
-	case 3:
-		document.write("three");
-		break;
-	case 4:
-		document.write("four");
-		break;
-	case 5:
-		document.write("five");
-		break;
-	case 6:
-		document.write("six");
-		break;
-	case 7:
-		document.write("seven");
-		break;
-	case 8:
-		document.write("eight");
-		break;
-	case 9:
-		document.write("nine");
-		break;
-	default:
-		document.write("not a digit");
-		break;
+// while 반복
+var i = 1;
+
+do {
+	document.write("멋쟁이사자 : " + i++ + "<br/>");
+} while (i <= 10);
+
+document.write("============<br/>");
+
+var j = 1;
+while (j <= 10) {
+	document.write("멋쟁이사자 : " + j++ + "<br/>");
 }
+
+// // switch
+// var digit = parseInt(prompt("Enter a digit : "));
+// switch (digit) {
+// 	case 0:
+// 		document.write("zero");
+// 		break;
+// 	case 1:
+// 		document.write("one");
+// 		break;
+// 	case 2:
+// 		document.write("two");
+// 		break;
+// 	case 3:
+// 		document.write("three");
+// 		break;
+// 	case 4:
+// 		document.write("four");
+// 		break;
+// 	case 5:
+// 		document.write("five");
+// 		break;
+// 	case 6:
+// 		document.write("six");
+// 		break;
+// 	case 7:
+// 		document.write("seven");
+// 		break;
+// 	case 8:
+// 		document.write("eight");
+// 		break;
+// 	case 9:
+// 		document.write("nine");
+// 		break;
+// 	default:
+// 		document.write("not a digit");
+// 		break;
+// }
 
 // // 모음 찾기
 // var letter = prompt("Enter a letter : ");
