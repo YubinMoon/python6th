8c6e1ed90fc503f1769fb256d594829ca39309b0
terry <munu220@gmail.com>
2023-05-31 10:04:37 +0900

    조건부 제어문

--- default.js
+++ default.js
@@ -1,23 +1,49 @@
-// 비교문
-var num1 = 20;
-var num2 = 10;
-var num3 = "10";
-var num4 = 20;
-var num5 = 15;
-console.log("일반 크기 비교");
-console.log(num1 > num2, num1, ">", num2);
-console.log(num1 >= num2, num1, ">=", num2);
-console.log(num1 < num2, num1, "<", num2);
-console.log(num1 <= num2, num1, "<=", num2);
-console.log("같은지 여부 확인");
-console.log(num1 == num4, num1, "==", num4);
-console.log(num1 != num4, num1, "!=", num4);
-console.log("===");
-console.log(num1 === num3, num1, "===", num3);
-console.log(num2 === num3, num2, "===", num3);
-console.log(num2 == num3, num2, "==", num3);
-console.log(num1 > num2 && num1 < num5, num1, ">", num2, "&&", num1, "<", num5);
-console.log(num1 > num2 || num1 < num5, num1, ">", num2, "||", num1, "<", num5);
+// 제어문
+var num1 = parseInt(prompt("첫번째 숫자 입력 : "));
+var num2 = parseInt(prompt("두번째 숫자 입력 : "));
+if (num1 > num2) {
+	document.write("큰 수 num1 : " + num1);
+}
+if (num1 < num2) {
+	document.write("큰 수 num2 : " + num2);
+}
+if (num1 == num2) {
+	document.write("같은 수");
+}
+if (num > num2) {
+	document.write("큰 수 num1 : " + num1);
+} else if (num1 < num2) {
+	document.write("큰 수 num2 : " + num2);
+} else if (num1 == num2) {
+	document.write("같은 수");
+}
+if (num > num2) {
+	document.write("큰 수 num1 : " + num1);
+} else if (num1 < num2) {
+	document.write("큰 수 num2 : " + num2);
+} else {
+	document.write("같은 수");
+}
+// // 비교문
+// var num1 = 20;
+// var num2 = 10;
+// var num3 = "10";
+// var num4 = 20;
+// var num5 = 15;
+// console.log("일반 크기 비교");
+// console.log(num1 > num2, num1, ">", num2);
+// console.log(num1 >= num2, num1, ">=", num2);
+// console.log(num1 < num2, num1, "<", num2);
+// console.log(num1 <= num2, num1, "<=", num2);
+// console.log("같은지 여부 확인");
+// console.log(num1 == num4, num1, "==", num4);
+// console.log(num1 != num4, num1, "!=", num4);
+// console.log("===");
+// console.log(num1 === num3, num1, "===", num3);
+// console.log(num2 === num3, num2, "===", num3);
+// console.log(num2 == num3, num2, "==", num3);
+// console.log(num1 > num2 && num1 < num5, num1, ">", num2, "&&", num1, "<", num5);
+// console.log(num1 > num2 || num1 < num5, num1, ">", num2, "||", num1, "<", num5);
 
 // // 온도 변환
 // var cels = parseFloat(prompt("섭씨 입력 : "));
