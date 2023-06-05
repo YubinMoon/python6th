fd985e3e9619ed3ff4e4522a560838b0a3839be3
terry <munu220@gmail.com>
2023-06-05 15:23:25 +0900

    출력 함수 실습

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-n5 = ("Kim", "Bae", "Park", "Lee")
-vn5 = list(n5)
+data = [10, 20, -50, 21.3, "LikeLion"]
+print(data)
 
-print(n5, type(n5))
-print(vn5, type(vn5))
+print("Like", "share", "Subscribe", sep="")
+print("Like", "share", "Subscribe", sep="***")
+print("Like", "share", "Subscribe", sep="***", end="\t")
+print("Like", "share", "Subscribe", end="\n")
+
+m = 40
+print("value: ", m)
+
+name = "이름"
+age = 22
+print("My name is", name, "and My age is", age)
+
+
+print("Welcome", end="\t")
+print("to", end="\t")
+print("LikeLion")
