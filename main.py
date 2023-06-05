691674d8a2f0762564a617290312017dfc5a94a3
terry <munu220@gmail.com>
2023-06-05 10:22:50 +0900

    파이썬 변수 실습

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+# 변수 선언
+a = 10
+b = 3.14
+c = "hello World!"
+d = [1, 2, 3]
+e = (4, 5, 6)
+f = {7, 8, 9}
+g = {"apple": 1, "banana": 2, "ortange": 3}
+
+# 데이터 타입 출력
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
