182c4efe8b48306b4f501a0049992a6a2674dd2f
terry <munu220@gmail.com>
2023-06-05 14:41:11 +0900

    문자열 -> 튜플

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-# 명시적 타입 변환
-a = 5
-b = 2
-value = a / b
-print(value, type(value))
-int_value = int(value)
-print(int_value, type(int_value))
+# 문자열 -> 튜플
 
-q = 20
-u = "10"
-print(u, type(u))
-r = q + int(u)
-print(r, type(r))
-r = str(q) + u
-print(r, type(r))
-
-n1 = 10.77
-vn1 = int(n1)
-
-print(vn1, type(vn1))
-
-n1 = 10
-vn1 = complex(n1)
-print(vn1, type(vn1))
+n5 = "멋쟁이 사자"
+vn5 = tuple(n5)
+print(vn5, type(vn5))
