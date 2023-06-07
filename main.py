345d7c422862583b5af710583f99d5ccfd37a865
terry <munu220@gmail.com>
2023-06-07 14:22:02 +0900

    문자열 함수

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-str1 = "LikeLion"
-str2 = """\
-동해물과 백두산이
-마르고 닳도록
-하느님이 보우하사
-우리나라 만세
-"""
-print(str1)
-print(str2)
-
-str3 = 'Hello "Like Lion" How are you'
-str4 = "Hello 'Like Lion' How are you"
-print(str3)
-print(str4)
-
-str5 = "Hello \nHow are you"
-str6 = "Hello \\nHow are you"
-str7 = r"Hello \nHow are you"
-print(str5)
-print(str6)
-print(str7)
+s = "Hello"
+print(s.upper())
+print(s.lower())
+print(s.swapcase())
+print(s.title())
+print(s.isupper())
+print(s.islower())
+print(s.istitle())
+print(s.isdigit())
+print(s.isalpha())
