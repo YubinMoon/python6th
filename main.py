ccf49cfe6440b8f67ec54209feaf083bbd9a3129
terry <munu220@gmail.com>
2023-06-08 10:15:04 +0900

    위치 인수, 키워드 인수 실습

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def dlsp(sh):
-    print(type(sh))
-    print("Dlsp Function", sh())
+def pw(x, y):
+    z = x**y
+    print(z)
 
 
-def show():
-    return " Show Function"
+pw(2, 5)
+# pw(5, 2, 3)  # ERROR
 
 
-dlsp(show)
+def show(name, age=27):
+    print(f"Name: {name} Age: {age}")
+
+
+show(name="멋쟁이사자", age=22)
+show(age=22, name="멋쟁이사자")
+show("멋쟁이사자")
+show("멋쟁이사자", 22)
+# show("멋쟁이사자", age=22, roll=12)  # ERROR
