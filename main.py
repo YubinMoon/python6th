4b3a5dc87e093655cf1b0cbfbfee1898fc07943b
terry <munu220@gmail.com>
2023-06-08 13:27:49 +0900

    generator alphabet

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-def fibonacci(n):
-    a, b = 0, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
+def generate_alphabet(start_letter, end_letter):
+    start = ord(start_letter)
+    end = ord(end_letter)
+    while start <= end:
+        yield chr(start)
+        start += 1
 
 
-runner = fibonacci(10)
-print(next(runner))
-print(next(runner))
-print(next(runner))
-for num in runner:
-    print(num)
+runner = generate_alphabet("a", "z")
+for letter in runner:
+    print(letter)
