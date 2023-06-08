937fd30be7bf9e2492f0079c61e653b791f93b22
terry <munu220@gmail.com>
2023-06-08 13:05:38 +0900

    generator fibonacci

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# 사용자 입력으로 리스트 만들기
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
 
-user_input_list = []
-num_elements = int(input("Enter Number of Elements: "))
-for i in range(num_elements):
-    user_input_list.append(input("Enter Element: "))
 
-print("User Input List:")
-for element in user_input_list:
-    print(element)
+runner = fibonacci(10)
+print(next(runner))
+print(next(runner))
+print(next(runner))
+for num in runner:
+    print(num)
