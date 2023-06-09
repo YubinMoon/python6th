d2ba9db766dc0071d5862cb7aee312d07e6a2460
terry <munu220@gmail.com>
2023-06-09 13:50:43 +0900

    파일 라인 읽기

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-with open("example.txt", "w") as file_object:
-    content = """this is a test file
-this is a test file
-this is a test file
-this is a test file
-"""
-    print(content)
-    file_object.write(content)
+with open("example.txt", "r") as file_object:
+    lines = file_object.readlines()
+    print(lines)
+    for line in lines:
+        print(line.strip())
