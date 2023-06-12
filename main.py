8cb9b0ad28511edd4715dddf39c4a024e4d2aec8
terry <munu220@gmail.com>
2023-06-12 13:04:47 +0900

    패키지 실습

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-class Engine:
-    def start(self):
-        return "Engine started"
-
-    def stop(self):
-        return "Engine stopped"
-
-
-class Wheels:
-    def rotate(self):
-        return "Wheels are rotating"
+def test():
+    try:
+        return "abc"
+    except:
+        return "aaa"
+    finally:
+        return "CCC"
 
 
-# 다중 상속
-class Car(Engine, Wheels):
-    pass
-
-
-# 인스턴스 생성
-my_car = Car()
-
-# 부모 클래스의 메소드 사용
-print(my_car.start())
-print(my_car.rotate())
+print(test())
