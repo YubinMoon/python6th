a1c25cc2a6058b198da2b12392aeaf408e9750b2
terry <munu220@gmail.com>
2023-06-16 11:36:10 +0900

    해시맵

--- main.py
+++ main.py
@@ -1,20 +1,62 @@
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        root = result = ListNode(None)
-        heap = []
+class ListNode:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.next = None
 
-        for i in range(len(lists)):
-            if lists[i]:
-                heapq.heappush(heap, (lists[i].val, i, lists[i]))
-        print(heapq)
 
-        while heap:
-            node = heapq.heappop(heap)
-            idx = node[1]
-            result.next = node[2]
+class MyHashMap:
+    def __init__(self):
+        self.size = 1000
+        self.table = collections.defaultdict(ListNode)
 
-            result = result.next
-            if result.next:
-                heapq.heappush(heap, (result.next.val, idx, result.next))
+    def put(self, key: int, value: int) -> None:
+        index = key % self.size
+        if self.table[index].value is None:
+            self.table[index] = ListNode(key, value)
+            return
 
-        return root.next
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                p.value = value
+                return
+            if p.next is None:
+                break
+            p = p.next
+        p.next = ListNode(key, value)
+
+    def get(self, key: int) -> int:
+        index = key % self.size
+        if self.table[index].value is None:
+            return -1
+
+        p = self.table[index]
+        while p:
+            if p.key == key:
+                return p.value
+            p = p.next
+        return -1
+
+    def remove(self, key: int) -> None:
+        index = key % self.size
+        if self.table[index].value is None:
+            return
+        p = self.table[index]
+        if p.key == key:
+            self.table[index] = ListNode() if p.next is None else p.next
+            return
+
+        prev = p
+        while p:
+            if p.key == key:
+                prev.next = p.next
+                return
+            prev, p = p, p.next
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
