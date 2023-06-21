0de3722b3d2131b2111dc338779831d971a62521
terry <munu220@gmail.com>
2023-06-21 14:37:35 +0900

    detail 추가

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from polls.models import Question
 
 
@@ -8,8 +8,9 @@ def index(request):
     return render(request, "polls/index.html", content)
 
 
-def detail(request):
-    pass
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def vote(request):
