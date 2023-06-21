2a0e644a3d5b146dc04ceeb081aad625dc1fc059
terry <munu220@gmail.com>
2023-06-21 15:15:10 +0900

    vote 함수 구현

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from polls.models import Question
+from django.urls import reverse
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -13,8 +15,20 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {"question": question})
 
 
-def vote(request):
-    pass
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "항목을 선택하지 않았습니다."},
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
 def results(request):
