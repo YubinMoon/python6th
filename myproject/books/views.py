66fe037bcb97729f415a235fbd9e8b06fe634409
terry <munu220@gmail.com>
2023-06-23 10:24:55 +0900

    index 생성

--- myproject/books/views.py
+++ myproject/books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class BooksModelView(TemplateView):
+    template_name = "books/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["model_list"] = ["Book", "Author", "Publisher"]
+        return context
