e19265241e3e4f46b3a6d13aa191e3ecfd34a11c
terry <munu220@gmail.com>
2023-06-23 15:08:06 +0900

    app.py 변경

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,11 +1,20 @@
-from typing import Any, Dict
+import logging
+
 from django.views.generic import TemplateView
+
+from django.apps import apps
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["app_list"] = ["polls", "books"]
+        # context['app_list'] = ['polls', 'books']
+        dictVerbose = {}
+        for app in apps.get_app_configs():
+            logging.getLogger("TEST").fatal(app.path)
+            if "site-packages" not in app.path:
+                dictVerbose[app.label] = app.verbose_name
+        context["verbose_dict"] = dictVerbose
         return context
--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,10 +1,20 @@
 import logging
+from django.views import generic
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from polls.models import Question, Choice
 
 logger = logging.getLogger(__name__)
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        # 최근 생성된 질문 5개를 반환함
+        return Question.objects.all().order_by("-pub_date")[:5]
 
 
 def index(request):
