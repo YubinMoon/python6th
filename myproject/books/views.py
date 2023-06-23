be24c4c3d2f758433bd7ce18e48180476124e7f1
terry <munu220@gmail.com>
2023-06-23 13:20:03 +0900

    author detail 생성

--- myproject/books/views.py
+++ myproject/books/views.py
@@ -26,3 +26,11 @@ class PublisherList(ListView):
 
 class BookDetail(DetailView):
     model = Book
+
+
+class AuthorDetail(DetailView):
+    model = Author
+
+
+class PublisherDetail(DetailView):
+    model = Publisher
